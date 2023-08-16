0c34dbe5e21766fb2ce11dbfe010853b0c9cd887
Aris Bello M <arisbellomartinez@gmail.com>
2023-08-16 18:20:50 -0500

    agrego css muy basico a otros elementos

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,15 @@ const Card = ({card}) => {
   return (
     card.map(
         (cartas, index) => (
-          <div key={index} >
+          <div key={index}style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%',
+            border: '1px solid black',
+          }}>
             <h3>Hola: {cartas.name}</h3>
             <p>Genero Musical Favorito:{cartas.genero}</p>
           </div>
